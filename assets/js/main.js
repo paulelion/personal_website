f76a4a687ceeb0f8884305e9cdf75093769fbac3
Paul Elion <paul.j.elion@gmail.com>
2018-02-05 12:23:40 -0500

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,14 +15,14 @@ $(document).ready(function(){
 
   // fullpage customization
   $('#fullpage').fullpage({
-    sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+    sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F', '#ffdab9'],
     sectionSelector: '.vertical-scrolling',
     slideSelector: '.horizontal-scrolling',
     navigation: true,
     slidesNavigation: true,
     css3: true,
     controlArrows: false,
-    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection'],
     menu: '#menu',
 
     afterLoad: function(anchorLink, index) {
